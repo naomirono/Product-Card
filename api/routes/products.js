a8524ff557b1fd7c5562740ae67c5a23d016caa7
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-17 22:45:41 +0300

    update product path

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,6 @@
 // routes/products.js
 const express = require('express');
-const Product = require('../models/Product');
+const Product = require('../models/Products');
 
 const router = express.Router();
 
