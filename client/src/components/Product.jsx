3f0263c90a6e0d303518482e829e355878918551
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-17 23:59:00 +0300

    Import FaHeart and FaEye icon

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { FaEye, FaHeart } from 'react-icons/fa';
 
 const Product = () => {
   const [products, setProducts] = useState([]);
@@ -43,18 +44,29 @@ const Product = () => {
 
   return (
     <div className="max-w-6xl mx-auto px-4 py-6">
-      <h2 className="text-3xl font-semibold text-center mb-6">Product Slider</h2>
+      <h2 className="text-4xl font-bold text-center mb-6 py-5">Latest <span className="text-orange-500"> Products</span></h2>
       <Slider {...sliderSettings}>
         {products.map(product => (
           <div key={product._id} className="p-2">
-            <div className="bg-white rounded-lg shadow-md p-4">
-              <img src={product.image} alt={product.title} className="w-full h-40 object-cover mb-2" />
-              <h3 className="text-gray-800 font-semibold">{product.title}</h3>
-              <p className="text-gray-600">{product.description}</p>
-              <div className="mt-4">
-                <span className="text-green-500 font-semibold">${product.price}</span>
-                <button className="ml-2 bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded">
+            <div className="bg-white rounded-lg shadow-md p-4 flex flex-col ">
+              <img 
+                src={product.image} 
+                alt={product.title} 
+                className="w-full h-40 object-cover mb-2"
+                style={{ transform: "rotate(-30deg)", height: "100%" }}
+              />
+              <h3 className="text-gray-800 font-semibold mb-2">{product.description}</h3>
+              
+              <p className="text-gray-600 font-semibold">${product.price}</p>
+              <div className="flex mt-2 space-x-2">
+                <button className="bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded">
                   Add to Cart
+                </button>
+                <button className="text-gray-500 hover:text-orange-500 ">
+                  <FaHeart />
+                </button>
+                <button className="text-gray-500 hover:text-orange-500 ">
+                  <FaEye />
                 </button>
               </div>
             </div>
