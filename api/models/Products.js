f470db98dc622748dc2301515dcd171e33b2cc18
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-17 21:38:14 +0300

    Create Product model schema in models/Product.js

---
+++ api/models/Products.js
@@ -0,0 +1,13 @@
+// models/Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  image: String,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
