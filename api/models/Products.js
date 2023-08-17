5134e7653d448b0e460d0de8206861985ed33f1e
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-17 23:55:15 +0300

    Change the price to strings

--- api/models/Products.js
+++ api/models/Products.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   title: String,
   description: String,
-  price: Number,
+  price: String,
   image: String,
 });
 
