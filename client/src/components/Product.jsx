a18ecdaa92dced54a2bc0e5a54f36fb0115816c2
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-18 08:59:27 +0300

    Update code to include star ratings

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { FaEye, FaHeart } from 'react-icons/fa';
+import { FaEye, FaHeart, FaStar } from 'react-icons/fa';
 
 const Product = () => {
   const [products, setProducts] = useState([]);
@@ -42,31 +42,51 @@ const Product = () => {
     ],
   };
 
+  const renderStars = (rating) => {
+    const starCount = 5;
+    const fullStarCount = Math.floor(rating);
+    const remainingStarCount = starCount - fullStarCount;
+
+    const stars = [];
+    for (let i = 0; i < fullStarCount; i++) {
+      stars.push(<FaStar key={`full-star-${i}`} className="text-yellow-500" />);
+    }
+    for (let i = 0; i < remainingStarCount; i++) {
+      stars.push(<FaStar key={`empty-star-${i}`} className="text-gray-300" />);
+    }
+
+    return stars;
+  };
+
   return (
     <div className="max-w-6xl mx-auto px-4 py-6">
-      <h2 className="text-4xl font-bold text-center mb-6 py-5">Latest <span className="text-orange-500"> Products</span></h2>
+      <h2 className="text-3xl font-semibold text-center mb-6">Product Slider</h2>
       <Slider {...sliderSettings}>
         {products.map(product => (
           <div key={product._id} className="p-2">
-            <div className="bg-white rounded-lg shadow-md p-4 flex flex-col ">
+            <div className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center">
               <img 
                 src={product.image} 
                 alt={product.title} 
                 className="w-full h-40 object-cover mb-2"
                 style={{ transform: "rotate(-30deg)", height: "100%" }}
               />
-              <h3 className="text-gray-800 font-semibold mb-2">{product.description}</h3>
-              
+              <h3 className="text-gray-800 font-semibold mb-2">{product.title}</h3>
+              <p className="text-gray-600">{product.description}</p>
               <p className="text-gray-600 font-semibold">${product.price}</p>
+              <div className="flex items-center space-x-1 mt-2">
+                {renderStars(product.rating)} {/* Display stars */}
+                <span className="text-gray-600">({product.reviews} reviews)</span>
+              </div>
               <div className="flex mt-2 space-x-2">
-                <button className="bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded mr-4">
+                <button className="bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded">
                   Add to Cart
                 </button>
-                <button className="text-gray-500 hover:text-orange-500 ">
-                  <FaHeart />
-                </button>
-                <button className="text-gray-500 hover:text-orange-500 ">
+                <button className="text-gray-500 hover:text-gray-700">
                   <FaEye />
+                </button>
+                <button className="text-gray-500 hover:text-red-700">
+                  <FaHeart />
                 </button>
               </div>
             </div>
