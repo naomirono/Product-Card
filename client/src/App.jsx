bdce21de9211595af768a7b30def6528550bd65f
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-20 14:12:01 +0300

    import featured.jsx and render it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { React } from 'react';
 import Product from './components/Product';
+import Featured from './pages/Featured';
 
 function App() {
 
   return (
     <>
       <Product/>
+      <Featured/>
     </>
   )
 }
