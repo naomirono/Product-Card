671612a19b632a9089d4a3cac55d47504ffc883b
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-20 16:52:54 +0300

    Import necessary components and set up routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { React } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Product from './components/Product';
 import Featured from './pages/Featured';
 
