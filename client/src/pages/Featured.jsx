d9ba34acf9ef0ab2787e2a8b355b32d6ebf41004
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-21 12:18:57 +0300

    Modify the Featured component to extract the product ID from the URL parameters using useParams and use it to display the selected product details.

--- client/src/pages/Featured.jsx
+++ client/src/pages/Featured.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FaStar } from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
 
 const Featured = () => {
+  const { productId } = useParams();
   const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   useEffect(() => {
     axios.get('http://localhost:3000/products')
@@ -14,6 +17,13 @@ const Featured = () => {
         console.error('Error fetching featured product:', error);
       });
   }, []);
+
+  useEffect(() => {
+    if (productId) {
+      const product = products.find(product => product._id === productId);
+      setSelectedProduct(product);
+    }
+  }, [productId, products]);
 
   const renderStars = (rating) => {
     const starCount = 5;
@@ -32,40 +42,41 @@ const Featured = () => {
   };
 
   return (
-    <div className='bg-gray-100'> 
-     {products.map(product => (
-          <div key={product._id} className="p-2"> 
-      <div className="max-w-7xl mx-auto px-4 py-12">
-        <div className=" flex flex-wrap gap-6 border border-solid border-gray-300 rounded bg-white p-8 items-center my-6 shadow-md">
-          <div className="image-container flex flex-1 items-center">
-            <div className="small-image w-2/6">
-            <img src={product.featuredImage1} alt="" className="featured-image-2 w-full border border-solid border-gray-300 cursor-pointer hover:bg-gray-300" />
-            <img src={product.featuredImage2} alt="" className="featured-image-2 w-full border border-solid border-gray-300 cursor-pointer hover:bg-gray-300" />
-            <img src={product.featuredImage3} alt="" className="featured-image-2 w-full border border-solid border-gray-300 cursor-pointer hover:bg-gray-300" />
-            <img src={product.featuredImage4} alt="" className="featured-image-2 w-full border border-solid border-gray-300 cursor-pointer hover:bg-gray-300" />
+    <div className='bg-gray-100'>
+      {selectedProduct && (
+        <div key={selectedProduct._id} className="p-2">
+          <div className="max-w-7xl mx-auto px-4 py-20">
+            <div className="flex flex-wrap gap-6 border border-solid border-gray-300 rounded bg-white p-8 items-center my-6 shadow-md">
+             <div className="image-container flex flex-1 items-center">
+               <div className="small-image w-2/6">
+                  <img src={selectedProduct.featuredImage1} alt="" className="featured-image-2 w-full border border-solid border-gray-300 cursor-pointer hover:bg-gray-300" />
+                  <img src={selectedProduct.featuredImage2} alt="" className="featured-image-2 w-full border border-solid border-gray-300 cursor-pointer hover:bg-gray-300" />
+                  <img src={selectedProduct.featuredImage3} alt="" className="featured-image-2 w-full border border-solid border-gray-300 cursor-pointer hover:bg-gray-300" />
+                  <img src={selectedProduct.featuredImage4} alt="" className="featured-image-2 w-full border border-solid border-gray-300 cursor-pointer hover:bg-gray-300" />
+               </div>
+               <div className="big-image w-3/4 p-2">
+                  <img src={selectedProduct.featuredImage0} alt="" className="big-image-2 w-full transform transition-transform hover:scale-105" />
+               </div>
+              </div>
+
+              <div className="content flex-1">
+                <h3 className="text-3xl font-semibold mb-2">{selectedProduct.description}</h3>
+                <div className="stars flex items-center space-x-1">
+                  {renderStars(selectedProduct.rating)}
+                </div>
+                <p className="text-gray-600 text-lg py-4">{selectedProduct.featuredDescription}</p>
+                <div className="price text-3xl font-semibold mb-2">
+                  ${selectedProduct.price} <span className="text-orange-500 text-lg line-through">${selectedProduct.featuredPrice}</span>
+                </div>
+                <button className="btn bg-blue-500 text-white py-2 px-4 rounded inline-block">
+                  Add to Cart
+                </button>
+              </div>
             </div>
-            <div className="big-image w-3/4 p-2">
-              <img src={product.featuredImage0} alt="" className="big-image-2 w-full transform transition-transform hover:scale-105" />
-            </div>
-          </div>
-          <div className="content flex-1">
-            <h3 className="text-3xl font-semibold mb-2">{product.description}</h3>
-            <div className="stars flex items-center space-x-1">
-            {renderStars(product.rating)}
-            </div>
-            <p className="text-gray-600 text-lg py-4">{product.featuredDescription}</p>
-            <div className="price text-3xl font-semibold mb-2">
-              ${product.price} <span className="text-orange-500 text-lg line-through">${product.featuredPrice}</span>
-            </div>
-            <button className="btn bg-blue-500 text-white py-2 px-4 rounded inline-block">
-              Add to Cart
-            </button>
           </div>
         </div>
-      </div>
-      </div>
-        ))}
-    </div>  
+      )}
+    </div>
   );
 };
 
