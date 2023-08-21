8b56d055aaad9feea3b2b46675ea76318fac5bb8
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-21 23:23:14 +0300

    Delete debugging console code

--- api/db.js
+++ api/db.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
-console.log(process.env.MONGODB_URI)
-
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
